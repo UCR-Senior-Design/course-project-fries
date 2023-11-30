d9222eb0528dbb68676130f661349669cc3ea635
Megan Chan <megantchan@gmail.com>
2023-11-30 09:43:18 -0800

    client to server data transfer works

--- backend/controllers/messages-controller.js
+++ backend/controllers/messages-controller.js
@@ -1,129 +1,30 @@
 const HttpError = require("../models/http-error");
 const { validationResult } = require("express-validator");
-const { v4: uuidv4 } = require("uuid");
-const Patient = require("../models/patient");
+const http = require("http");
+// const WebSocketServer = require("ws");
+const ws = require("ws");
 
-let DUMMY_USERS = [
-  {
-    id: "p1",
-    firstname: "Megan",
-    lastname: "Chan",
-    role: "doctor",
-    doctor_id: "",
-  },
-  {
-    id: "p2",
-    firstname: "Patient A",
-    lastname: "Lastname A",
-    role: "patient",
-    doctor_id: "p1",
-  },
-  {
-    id: "p3",
-    firstname: "Patient B",
-    lastname: "Lastname B",
-    role: "patient",
-    doctor_id: "p1",
-  },
-  {
-    id: "p4",
-    firstname: "Patient C",
-    lastname: "Lastname C",
-    role: "patient",
-    doctor_id: "p5",
-  },
-  {
-    id: "p5",
-    firstname: "Doctor A",
-    lastname: "Lastname DrA",
-    role: "doctor",
-    doctor_id: "",
-  },
-];
-
-// GET user by uid
-const get_user_by_uid = (req, res, next) => {
-  const user_id = req.params.uid;
-  const user = DUMMY_USERS.find((p) => {
-    return p.id == user_id;
-  });
-  if (!user) {
-    return next(new HttpError("Could not find user by provided uid.", 404));
-  }
-  res.json({ user: user }); // {user:user} == {user}
-};
-
-// GET list of patients by doctor uid
-const get_patientlist_by_dr_id = (req, res, next) => {
-  const doctor_id = req.params.doctor_id;
-  const patient_list = DUMMY_USERS.filter((p) => {
-    return p.doctor_id === doctor_id;
+const start_server = () => {
+  // Attach WebSocket Server instance to HTTP server instance
+  const server = http.createServer();
+  const wss = new ws.WebSocketServer({ server });
+  const port = 8080;
+  server.listen(port, () => {
+    console.log(`WebSocket server is running on port ${port}`);
   });
 
-  if (!patient_list || patient_list.length === 0) {
-    return next(
-      new HttpError("Could not find patients list for provided doctor id.", 404)
-    );
-  }
-
-  res.json({ patient_list });
+  // New client connection request received
+  wss.on("connection", function (connection) {
+    console.log("Server received a new connection.");
+    connection.send("Connected to WebSocket server!"); // TODO: doesnt do anything
+    // Event listener for messages
+    connection.on("message", (message) => {
+      // Echoes message back to client
+      // connection.send(`Message from server: ${message}`);
+      connection.send(message.content);
+      console.log("Server received message from client: %s", message);
+    });
+  });
 };
 
-// POST User
-const create_user = (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    throw new HttpError("Invalid inputs passed, check your data.", 422);
-  }
-
-  const { id, firstname, lastname, role, doctor } = req.body;
-  const { doctor_id } = doctor;
-  const created_user = {
-    id: uuidv4(),
-    firstname,
-    lastname,
-    role,
-    doctor_id,
-  };
-
-  DUMMY_USERS.push(created_user);
-  res.status(201).json({ created_user });
-};
-
-// PATCH User
-const patch_user = (req, res, next) => {
-  const { firstname, lastname, doctor_id } = req.body;
-  const user_id = req.params.uid;
-
-  const updated_user = { ...DUMMY_USERS.find((p) => p.id == user_id) }; // Make a copy of user with given id
-  const user_index = DUMMY_USERS.findIndex((p) => p.id === user_id); // Get index of user in DUMMY_USERS
-
-  updated_user.firstname = firstname;
-  updated_user.lastname = lastname;
-  updated_user.doctor_id = doctor_id;
-
-  DUMMY_USERS[user_index] = updated_user;
-
-  if (!updated_user) {
-    return next(new HttpError("Could not find user by provided uid.", 404));
-  }
-
-  res.status(200).json({ user: updated_user });
-};
-
-// DELETE User
-const delete_user = (req, res, next) => {
-  const user_id = req.params.uid;
-  if (!DUMMY_USERS.find((p) => p.id === user_id)) {
-    throw new HttpError("Could not find user", 404);
-  }
-  DUMMY_USERS = DUMMY_USERS.filter((p) => p.id !== user_id);
-  res.status(200).json({ message: "Deleted user" });
-};
-
-// Export pointer to functions for router
-exports.get_user_by_uid = get_user_by_uid;
-exports.get_patientlist_by_dr_id = get_patientlist_by_dr_id;
-exports.create_user = create_user;
-exports.patch_user = patch_user;
-exports.delete_user = delete_user;
+exports.start_server = start_server;
--- frontend/src/messages/pages/Messages.js
+++ frontend/src/messages/pages/Messages.js
@@ -1,7 +1,69 @@
 import React from "react";
+import useWebSocket from "react-use-websocket";
+import { useState, useEffect } from "react";
+//import styles from "./Messages.module.css";
 
 const Messages = () => {
-  return <h1>Messages Page</h1>;
+  const [enteredMessage, setEnteredMessage] = useState("");
+
+  // Connect to server
+  const ws_URL = "ws://localhost:8080";
+  const { sendJsonMessage, lastJsonMessage } = useWebSocket(ws_URL, {
+    onOpen: () => {
+      console.log("WebSocket connection established.");
+    },
+  });
+
+  // Listen for messages from the server
+  useEffect(() => {
+    console.log("lastJsonMessage: ", lastJsonMessage);
+    if (lastJsonMessage) {
+      console.log("received message from server: ", lastJsonMessage);
+    }
+  }, [lastJsonMessage]);
+
+  // Set entered message into a variable
+  const entered_message_handler = (event) => {
+    setEnteredMessage(event.target.value);
+  };
+
+  // Send messages to server
+  const send_message = () => {
+    console.log("Send this message: " + enteredMessage);
+    sendJsonMessage({
+      type: "clientmessage",
+      content: enteredMessage,
+    });
+  };
+
+  // Submit Form
+  const form_submit_handler = async (event) => {
+    event.preventDefault();
+    console.log("form_submit_handler() works!");
+    send_message();
+    setEnteredMessage("");
+  };
+
+  return (
+    <div>
+      <h1>Messages Page</h1>
+      <link></link>
+      <script defer src="app.js"></script>
+      <div>
+        <form onSubmit={form_submit_handler}>
+          {" "}
+          <input
+            type="text"
+            placeholder="Your Message"
+            value={enteredMessage}
+            onChange={entered_message_handler}
+          ></input>
+          <button>Send</button>
+        </form>
+        <ul></ul>
+      </div>
+    </div>
+  );
 };
 
 export default Messages;
