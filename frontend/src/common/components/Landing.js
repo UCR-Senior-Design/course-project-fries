11a3b8b0d937393189d4f2fa95d8f3e93e133060
Dave Quichocho <50337850+quichochodd@users.noreply.github.com>
2024-03-18 22:14:21 -0700

    Update Landing.js

--- frontend/src/common/components/Landing.js
+++ frontend/src/common/components/Landing.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './landing.css';
-import medshareLanding from './medshareLanding.jpeg';
-import landingpage from './landingpage.jpeg';
-import landingpt2 from './landingpt2.jpeg';
+import landing from './landing.jpeg';
 
 const Landing = () => {
   return (
     <div class="image-container">
-      <img src={landingpt2} class="background-image" />
+      <img src={landing} class="background-image" />
     <div class="text-container">
     <h1>Welcome To MedShare</h1>
     <p>Medicine On The Go!</p>
